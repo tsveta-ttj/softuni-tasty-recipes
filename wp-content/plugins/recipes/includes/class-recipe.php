4ec0beac05acf279d73b898775e2542d5b9a36df
tsveta-ttj <tzvety.ty@gmail.com>
2024-02-19 14:12:15 +0200

    Code clean up

--- wp-content/plugins/recipes/includes/class-recipe.php
+++ wp-content/plugins/recipes/includes/class-recipe.php
@@ -1,5 +1,5 @@
 <?php
-if (!class_exists( 'Su_Recipes' )) :
+if ( !class_exists( 'Su_Recipes' ) ) :
 
     /**
      * This is our Recipe Class for registering Recipe Custom Type and taxonomy for it.
@@ -8,13 +8,12 @@ if (!class_exists( 'Su_Recipes' )) :
         public function __construct() {
             // Register the CPT and taxonomies
             add_action( 'init', array( $this, 'recipes_cpt' ) );
-            add_action( 'init', array( $this, 'recipes_category_taxonomy' ));
-                    
+            add_action( 'init', array( $this, 'recipes_category_taxonomy' ) );
             // Register Metaboxes
-            add_action( 'add_meta_boxes', array( $this, 'recipe_register_meta_boxes' ));
+            add_action( 'add_meta_boxes', array( $this, 'recipe_register_meta_boxes' ) );
 
             // Save Actions
-            add_action('save_post', array( $this, 'recipe_meta_save'));
+            add_action( 'save_post', array( $this, 'recipe_meta_save' ) );
         }
 
         /**
@@ -27,11 +26,11 @@ if (!class_exists( 'Su_Recipes' )) :
                 'name'          => _x( 'Recipes', 'Post type general name', 'softuni' ),
                 'singular_name' => _x( 'Recipe', 'Post type singular name', 'softuni' ),
                 'menu_name'     => _x( 'Recipes', 'Admin Menu text', 'softuni' ),
-                'name_admin_bar'=> _x( 'Recipe', 'Add New on Toolbar', 'softuni' ),
+                'name_admin_bar' => _x( 'Recipe', 'Add New on Toolbar', 'softuni' ),
                 'add_new'       => __( 'Add New', 'softuni' ),
                 'add_new_item'  => __( 'Add New Recipe', 'softuni' ),
                 'new_item'      => __( 'New Recipe', 'softuni' ),
-                'edit_item'     => __ ('Edit Recipe', 'softuni' ),
+                'edit_item'     => __( 'Edit Recipe', 'softuni' ),
                 'view_item'     => __( 'View Recipe', 'softuni' ),
                 'all_items'     => __( 'All Recipes', 'softuni' ),
             );
@@ -51,7 +50,7 @@ if (!class_exists( 'Su_Recipes' )) :
                 'show_in_rest'       => true
             );
 
-            register_post_type('recipe', $args);
+            register_post_type( 'recipe', $args );
         }
 
         /**
@@ -59,7 +58,7 @@ if (!class_exists( 'Su_Recipes' )) :
          * 
          * @return void 
          */
-        public function recipes_category_taxonomy(){
+        public function recipes_category_taxonomy() {
 
             $labels = array(
                 'name'          => 'Categories',
@@ -71,44 +70,44 @@ if (!class_exists( 'Su_Recipes' )) :
                 'show_in_rest' => true,
             );
 
-            register_taxonomy('recipes_category', 'recipe', $args);
+            register_taxonomy( 'recipes_category', 'recipe', $args );
         }
 
         /**
-        * Register meta box(es).
-        */
-        public function recipe_register_meta_boxes(){
-            add_meta_box('gluten_free', __('Is Gluten free?', 'softuni'), array( $this, 'recipe_featured_metabox_callback' ), 'recipe', 'side');
+         * Register meta box(es).
+         */
+        public function recipe_register_meta_boxes() {
+            add_meta_box( 'gluten_free', __('Is Gluten free?', 'softuni'), array($this, 'recipe_featured_metabox_callback'), 'recipe', 'side' );
         }
 
         /**
-        * Callback function for my Featured metabox
-        * 
-        * @return void
-        */
+         * Callback function for my Featured metabox
+         * 
+         * @return void
+         */
         function recipe_featured_metabox_callback( $post_id ) {
             $checked = get_post_meta( $post_id->ID, 'is_gluten_free', true );
-        ?>
+?>
             <div>
                 <label for='isglutenfree'>Is Gluten free?</label>
-                <input id='is-gluten-free' name='isglutenfree' type='checkbox' value='1' <?php checked( $checked, 1, true ); ?>/>
+                <input id='is-gluten-free' name='isglutenfree' type='checkbox' value='1' <?php checked( $checked, 1, true ); ?> />
             </div>
-        <?php
+<?php
         }
-    
+
         /**
          * Function to update my Featured metabox
          * 
          * @return void
          */
         public function recipe_meta_save( $post_id ) {
-            if  (empty( $post_id ) ) {
+            if ( empty( $post_id ) ) {
                 return;
             }
 
             $gluten = '';
 
-            if ( isset( $_POST['isglutenfree']) ) {
+            if ( isset( $_POST['isglutenfree'] ) ) {
                 $gluten = $_POST['isglutenfree'];
             }
 
